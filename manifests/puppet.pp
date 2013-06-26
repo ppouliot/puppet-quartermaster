f3a0d46780abf3e594ecf0f737eb30c6c9e76099
Peter J. Pouliot <ppouliot@gmail.com>
2013-06-26 13:41:49 -0400

    fixing puppet inclusion for puppetmaster support

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -1,13 +1,6 @@
 class quartermaster::puppet {
-#include 'puppet'
-
-  class {'passenger':
-    passenger_version => '3.0.19',
-    passenger_provider => 'apt',
-  }
+ include 'puppet'
 
   class {'puppet::master':
-    puppet_passenger => "true",
-    require          => Class["passenger"],
   } 
 }
