a495326d7da9486f3cdda74e8feab0169eea6b76
Peter J. Pouliot <ppouliot@gmail.com>
2013-07-10 18:39:52 -0400

    class to help automate overall provisioning

---
+++ manifests/bootstrap.pp
@@ -0,0 +1,13 @@
+class quartermaster::bootstrap {
+
+  file { '/etc/puppet/manifests/site.pp':
+    ensure => present,
+    content => template('quartermaster/site.erb'),
+  }
+
+  exec {'default_site_init':
+    command => '/usr/bin/puppet apply --debug --trace --verbose /etc/puppet/manifests/site.pp',
+    require => File['/etc/puppet/manifests/site.pp'],
+  }
+
+}
