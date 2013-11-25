b899f321e5aab3026d06fc7ad5e30cde5a92350f
Peter J. Pouliot <ppouliot@gmail.com>
2013-11-24 23:37:15 -0500

    trying to get bootstrap.pp working in a dockerfile

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -7,7 +7,7 @@ class quartermaster::bootstrap {
 
   exec {'default_site_init':
 #    command => '/usr/bin/puppet agent --debug --trace --verbose --test --server ${fqdn} --waitforcert=60',
-    command => '/usr/bin/puppet apply --debug --trace --verbose --test --modulepath=/etc/puppet/modules',
+    command => '/usr/bin/puppet apply --debug --trace --verbose --modulepath=/etc/puppet/modules /etc/puppet/manifests/site.pp',
     require => File['/etc/puppet/manifests/site.pp'],
     timeout => 0,
     logoutput => true,
