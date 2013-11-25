cc038c0587680e098c7a3524e1ac7757545f9c65
Peter J. Pouliot <ppouliot@gmail.com>
2013-11-24 22:36:49 -0500

    fixing bootstrap

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -6,7 +6,8 @@ class quartermaster::bootstrap {
   }
 
   exec {'default_site_init':
-    command => '/usr/bin/puppet agent --debug --trace --verbose --test --server ${fqdn} --waitforcert=60',
+#    command => '/usr/bin/puppet agent --debug --trace --verbose --test --server ${fqdn} --waitforcert=60',
+    command => '/usr/bin/puppet apply --debug --trace --verbose --test --modulepath=/etc/puppet/modules',
     require => File['/etc/puppet/manifests/site.pp'],
     timeout => 0,
     logoutput => true,
