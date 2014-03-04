950d64d7fe2a001593381ab1bed32003bcff0918
Peter J. Pouliot <ppouliot@gmail.com>
2014-03-04 14:16:56 -0800

    modifying script to adjust for upstream changes

--- files/librarian-puppet-bootstrap.sh
+++ files/librarian-puppet-bootstrap.sh
@@ -6,7 +6,7 @@ echo '### UPDATING AND INSTALLING NECESSARY PACKAGES ###'
 apt-get update -y && apt-get install -y openssh-server git puppet ruby1.9.1 ruby1.9.1-dev rubygems
 
 echo "### INSTALLING Q's PUPPETFILE INTO /etc/puppet ###"
-wget https://raw.github.com/ppouliot/ppouliot-quartermaster/master/Puppetfile -O /etc/puppet/Puppetfile
+wget https://raw.github.com/openstack-hyper-v/puppet-quartermaster/master/Puppetfile -O /etc/puppet/Puppetfile
 
 echo "### INSTALLING  LIBRARIAN-PUPPET-MAESTRODEV DUE TO BUGFIXES IN LIBRARIAN-PUPPET ###"
 gem install librarian-puppet
@@ -16,4 +16,3 @@ cd /etc/puppet && librarian-puppet install --verbose
  
 echo "### BOOTSTRAPPING QUARTERMASTER ###"
 puppet apply --verbose --trace --debug --modulepath=/etc/puppet/modules /etc/puppet/modules/quartermaster/tests/init.pp
-
