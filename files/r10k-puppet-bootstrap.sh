3a7c841ff9879bac303f33f4ff6b91fa4a5fc702
primeministerp <primeministerpete@gmail.com>
2014-03-17 11:20:17 -0400

    fixing r10k bootstrap and hiera loading

--- files/r10k-puppet-bootstrap.sh
+++ files/r10k-puppet-bootstrap.sh
@@ -8,10 +8,10 @@ apt-get update -y && apt-get install -y openssh-server git puppet ruby1.9.1 ruby
 echo "### INSTALLING Q's PUPPETFILE INTO /etc/puppet ###"
 wget https://raw.github.com/openstack-hyper-v/puppet-quartermaster/master/Puppetfile -O /etc/puppet/Puppetfile
 
-echo "### INSTALLING  LIBRARIAN-PUPPET DUE TO BUGFIXES IN LIBRARIAN-PUPPET ###"
+echo "### INSTALLING  R10K ###"
 gem install r10k
 
-echo "### RUNNING LIBRARIAN_PUPPET ###"
+echo "### RUNNING R10K ###"
 cd /etc/puppet && r10k --verbose DEBUG puppetfile install
 
 #Setting Minimal hiera for QuarterMaster
