e2c991e212edfd6e0df1065c2c63369344c0c744
primeministerp <primeministerpete@gmail.com>
2014-12-15 09:49:06 -0500

    adding nfs

---
+++ manifests/server/nfs.pp
@@ -0,0 +1,28 @@
+# Class: quartermaster::nfs
+#
+# This Class and configures installs nfs on the quartermaster server
+#
+
+class quartermaster::nfs (
+  $nfsroot = $quartermaster::params::nfsroot,
+) inherits quartermaster::params {
+
+  notify {'Exporting Installation Directories via NFS':}
+  include nfs::server
+  nfs::server::export{ $nfsroot:
+    ensure  => 'mounted',
+    clients => '*(rw,insecure,all_squash,no_subtree_check,nohide)',
+  }
+
+  file { ["${nfsroot}",
+         "${nfsroot}\hosts",
+         "${nfsroot}\hosts\hiera",
+         "${nfsroot}\hosts\pxefiles",
+         "${nfsroot}\hardware"]:
+    ensure  => 'directory',
+    owner   => 'nobody',
+    group   => 'nogroup',
+    mode    => '0755',
+  }
+
+}
