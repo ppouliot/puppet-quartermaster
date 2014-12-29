bd97d1caf6e2fa613499ee9dc08e08b0cffe0652
Peter J. Pouliot <ppouliot@gmail.com>
2014-12-29 14:39:36 +0000

    fixing error by importing tftp::params in our params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,8 @@
 class quartermaster::params {
+
+  # Include Params from other modules so we can use them here
+  include tftp::params
+
   $arp_type = '01'
   $host_macaddress = regsubst($macaddress, '(\:)','-','G')
   $default_pxeboot_option = 'menu.c32'
@@ -7,7 +11,7 @@ class quartermaster::params {
   $tmp               = '/tmp'
   $logroot           = '/var/log/quartermaster'
 #  $tftpboot          = '/srv/quartermaster/tftpboot'
-  $tftpboot          = $::tftp::params::directory
+  $tftpboot          = $tftp::params::directory
   $wwwroot           = '/srv/quartermaster/install'
   $nfsroot           = '/srv/quartermaster/nfs'
   $bin               = "${wwwroot}/bin"
