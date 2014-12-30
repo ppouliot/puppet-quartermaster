a173762600dd0cc5a16c21bdfce89a3f89661fac
Peter J. Pouliot <ppouliot@gmail.com>
2014-12-30 16:15:15 +0000

    update to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@ class quartermaster::params {
 
   # Include Params from other modules so we can use them here
   include tftp::params
+  include apache::params
 
   $arp_type = '01'
   $host_macaddress = regsubst($macaddress, '(\:)','-','G')
