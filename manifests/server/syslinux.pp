438235075cc119afd8fb481977e86ecd792e54f6
Peter J. Pouliot <ppouliot@gmail.com>
2015-01-12 20:45:34 +0000

    continued refactoring

--- manifests/server/syslinux.pp
+++ manifests/server/syslinux.pp
@@ -10,22 +10,45 @@ class quartermaster::server::syslinux (
   $pxeroot      = $quartermaster::params::pxeroot,
   $pxecfg       = $quartermaster::params::pxecfg,
   $pxe_menu     = $quartermaster::params::pxe_menu,
+  $syslinux     = $quartermaster::params::syslinux,
   $syslinux_url = $quartermaster::params::syslinux_url,
   $syslinux_ver = $quartermaster::params::syslinux_ver,
 
 ) inherits quartermaster::params {
 
-  # Syslinux Staging
- # staging::file { "syslinux-${syslinux_ver}.tar.xz":
-  staging::deploy { "syslinux-${syslinux_ver}.tar.xz":
-    source => "${syslinux_url}/syslinux-${syslinux_ver}.tar.gz"
+  # Syslinux Staging and Extraction
+  staging::deploy { "${syslinux}.tar.gz":
+    source => "${syslinux_url}/${syslinux}.tar.gz",
     target  => $tmp,
+    creates => "${tmp}/${syslinux}",
   }
-  # Syslinux Extraction
-#  staging::extract { "syslinux-${syslinux_ver}.tar.xz":
-#    target  => "${tmp}/${syslinux}",
-#    creates => "${tmp}/${syslinux}",
-#    require => Staging::File["syslinux-${syslinux_ver}.tar.gz"],
-#  }
+  Tftp::File{
+    owner   => 'tftp',
+    group   => 'tftp',
+    require => [
+      Staging::Deploy["${syslinux}.tar.gz"],
+      Tftp::File['pxelinux']
+    ],
+  }
+
+  tftp::file {'pxelinux/pxelinux.0':
+    source  => "${tmp}/${syslinux}/core/pxelinux.0",
+  }
+
+  tftp::file { 'pxelinux/gpxelinux0':
+    source  => "${tmp}/${syslinux}/gpxe/gpxelinux.0",
+  }
+
+  tftp::file { 'pxelinux/isolinux.bin':
+    source  => "${tmp}/${syslinux}/core/isolinux.bin",
+  }
+
+  tftp::file { 'pxelinux/menu_c32':
+    source  => "${tmp}/${syslinux}/com32/menu/menu.c32",
+  }
+
+  tftp::file { 'pxelinux/ldlinux_c32':
+    source  => "${tmp}/${syslinux}/com32/elflink/ldlinux/ldlinux.c32",
+  }
 
 }
