1d8829009ae76711e4f099fccff6dd06afbeb1dc
primeministerp <primeministerpete@gmail.com>
2015-01-27 15:23:56 -0500

    trying to fix bug

--- manifests/pxe.pp
+++ manifests/pxe.pp
@@ -320,11 +320,12 @@ define quartermaster::pxe {
 
 # Distro Specific TFTP Graphics.conf 
 
-if ! defined (Tftp::File["${distro}/menu/${name}"]) {
+#if ! defined (Tftp::File["${distro}/menu/${name}"]) {
   tftp::file { "${distro}/menu/${name}.graphics.conf":
     content => template("quartermaster/pxemenu/${linux_installer}.graphics.erb"),
+    require => Tftp::File["${distro}/menu"],
   }
-}
+#}
 
 # Begin Creating Distro Specific HTTP Folders
 
