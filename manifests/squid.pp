464f81adeacf648cf0093f0bf4064875f994d35b
Peter J. Pouliot <ppouliot@gmail.com>
2015-02-05 10:25:56 -0500

    testing dhcp_proxy_subnets as inline variable w/o interpolation of variables

--- manifests/squid.pp
+++ manifests/squid.pp
@@ -7,6 +7,15 @@
 #
 #
 class quartermaster::squid () inherits quartermaster::params {
+
+ if (${operatingsystem} == 'Ubuntu') and (${operatingsystemrelease} > '14.04') {
+    Service{'squid3':
+      provider => 'upstart',
+    }
+ }
+
+
+
   class {'squid3':
     template   => ('quartermaster/squid.conf.erb'),
 #    acl        => [
