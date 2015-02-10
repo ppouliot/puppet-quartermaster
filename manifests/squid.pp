7fca5e829c84610eaa404fa282a65150702f23cf
primeministerp <primeministerpete@gmail.com>
2015-02-10 16:41:58 -0500

    trying to adjust format of this statement

--- manifests/squid.pp
+++ manifests/squid.pp
@@ -9,7 +9,7 @@
 class quartermaster::squid () inherits quartermaster::params {
 
 
- if ($operatingsystem == 'Ubuntu') and ($operatingsystemrelease => '14.04') {
+ if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease == '14.04' {
     service{'squid3':
       provider => 'upstart',
     }
