2975bf78748e0415c6c721d526fb69a88667571a
Peter J. Pouliot <ppouliot@gmail.com>
2015-02-11 12:17:47 -0500

    fixing squid

--- manifests/squid.pp
+++ manifests/squid.pp
@@ -8,6 +8,12 @@
 #
 class quartermaster::squid () inherits quartermaster::params {
 
+  if (${operatingsystem} == 'Ubuntu') and (${operatingsystemrelease} > '14.04') {
+    Service{'squid3_service':
+      provider => 'upstart',
+    }
+  }
+
 
   class {'squid3':
     template   => ('quartermaster/squid.conf.erb'),
