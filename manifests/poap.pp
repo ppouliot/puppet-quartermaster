113ae58d5b61e50410ba63bdc3fccfaee089afec
primeministerp <primeministerpete@gmail.com>
2015-03-09 21:29:16 -0400

    adding examples from cisco

--- manifests/poap.pp
+++ manifests/poap.pp
@@ -14,16 +14,53 @@ class quartermaster::poap (
 
     file{["${wwwroot}/nexus",
           "${wwwroot}/nexus/poap",]:
+          "${wwwroot}/nexus/scripts",]:
       ensure => directory,
     }
     tftp::file{'nexus':
       ensure => directory,
     }
 
-    file{"${wwwroot}/nexus/poap/config.py":
+    file{"${wwwroot}/nexus/scripts/config.py":
       ensure => file,
       content => template("quartermaster/poap.config.py.erb"),
     }
+
+   vcsrepo{"${wwwroot}/nexus/scripts/PyMonitor":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/PyMonitor',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/link-state-monitor":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/link-state-monitor',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/nexus5000":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/nexus5000',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/nexus9000":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/nexus9000',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/ABM-Beam":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/ABM-Beam',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/hadoop-integration":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/hadoop-integration',
+   }
+   vcsrepo{"${wwwroot}/nexus/scripts/who-moved-my-cli":
+     ensure   => present,
+     provider => git,
+     source   => 'https://github.com/datacenter/who-moved-my-cli',
+   }
 
   } else {
 
