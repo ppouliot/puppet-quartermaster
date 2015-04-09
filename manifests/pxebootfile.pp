4c1953337f4d8ec267d3bfc27909e0250cd744ed
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-09 16:56:38 -0400

    pxebootfile.pp

--- manifests/pxebootfile.pp
+++ manifests/pxebootfile.pp
@@ -6,7 +6,7 @@
 #
 define quartermaster::pxebootfile (
   $interface_name         = split($interfaces,",")
-  $interface_macaddr      = inline_template('$macaddress_'"${interface_name}")
+  $interface_macaddr      = inline_template("\$macaddress_${interface_name}")
   $arp_type               = $quartermaster::params::arp_type,
   $default_pxeboot_option = $quartermaster::params::default_pxe_option,
   $pxe_menu_timeout       = $quartermaster::params::pxe_menu_timeout,
@@ -17,7 +17,7 @@ define quartermaster::pxebootfile (
   #$host_macaddress = regsubst($macaddress, '(\:)','-','G')
 
   case $interface_name != 'lo' {
-    @@file{"${tftpboot}/pxelinux/pxelinux.cfg/${arptype}-$interface_macaddr":
+    @@file{"${tftpboot}/pxelinux/pxelinux.cfg/${arptype}-${interface_macaddr}":
       ensure = file,
     }
   }
