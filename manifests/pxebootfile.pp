7cf6f067f2fd751815bc25ad1bc65900324e39c4
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-09 16:01:08 -0400

    removing typo

--- manifests/pxebootfile.pp
+++ manifests/pxebootfile.pp
@@ -7,10 +7,9 @@
 define quartermaster::pxebootfile (
   $arp_type               = $quartermaster::params::arp_type,
   $host_macaddress        = $quartermaster::params::host_macaddress,
-  $default_pxeboot_option = $quartermaster::params::default_pxe_option
-  $pxe_menu_timeout       = $quartermaster::params::pxe_menu_timeout
-  
-) {
+  $default_pxeboot_option = $quartermaster::params::default_pxe_option,
+  $pxe_menu_timeout       = $quartermaster::params::pxe_menu_timeout,
+){
 
 
   #$host_macaddress = regsubst($macaddress, '(\:)','(\-)','G')
@@ -18,44 +17,10 @@ define quartermaster::pxebootfile (
 
   $bootfile = ${arp_type}-${host_macaddress}"
 
-  case $osfamily {
-    'Debian':{
+case $interfaces != 'lo'{
 
-    }
-    'RedHat':{}
-    'windows':{}
-    default:{}
+  @@file{"${tftpboot}/pxelinux/pxelinux.cfg/${bootfile}":
+    ensure = file,
   }
-
-
-
-
-
-  ## creates files tftpboot/pxelinux/pxelinux.cfg/
-  @@concat { "${tftpboot}/pxelinux/pxelinux.cfg/${bootfile}":
-    mode    => $tftp_filemode,
-  }
-    
-  ## Set a Default target for concat 
-  @@Concat:Fragment{
-    target  => "${tftpboot}/pxelinux/pxelinux.cfg/boot",
-  }
-
-  ## Bootfile Header 
-  @@concat::fragment{"$bootfile.header":
-    content => template("quartermaster/pxemenu/header.erb"),
-    order   => 01,
-  }
-
-  ## Localboot - Boot from the local disk
-  @@concat::fragment{"$bootfile.localboot":
-    content => template("quartermaster/pxemenu/localboot.erb"),
-    order   => 02,
-  }
-  ## Puppet Fact Detected Menu Option
-#  @@concat::fragment{"$bootfile.operatingsystem":
-#    content => template("quartermaster/pxemenu/localboot.erb"),
-#    order   => 02,
-#  }
-
+}
 }
