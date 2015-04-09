95a2bae3bf38f6229182be95920c90d3719e1bc4
Peter J. Pouliot <ppouliot@gmail.com>
2015-04-09 16:40:57 -0400

    trying to iterate interface

--- manifests/pxebootfile.pp
+++ manifests/pxebootfile.pp
@@ -8,7 +8,6 @@ define quartermaster::pxebootfile (
   $interface_name         = split($interfaces,",")
   $interface_macaddr      = inline_template("\$macaddress_${interface_name})
   $arp_type               = $quartermaster::params::arp_type,
-  $host_macaddress        = inline_template(macaddress_$fs{}),
   $default_pxeboot_option = $quartermaster::params::default_pxe_option,
   $pxe_menu_timeout       = $quartermaster::params::pxe_menu_timeout,
 ){
