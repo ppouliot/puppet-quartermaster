fa6fc7af3c07b4b35c48709c31044981066250bb
primeministerp <primeministerpete@gmail.com>
2015-09-08 11:07:14 -0400

    adding directory structure for hp_spp

--- manifests/hp_spp.pp
+++ manifests/hp_spp.pp
@@ -3,7 +3,36 @@
 class quartermaster::hp_spp (
   $hp_spp_iso_complete_url_location = 'ftp://ftp.hp.com/pub/softlib2/software1/cd-generic/p67859018/v108240/SPP2015040.2015_0407.5.iso',
   $hp_spp_iso_name = 'SPP2015040.2015_0407.5.iso',
+
+  $tmp            = $quartermaster::params::tmp,
+  $pxeroot        = $quartermaster::params::pxeroot,
+  $pxecfg         = $quartermaster::params::pxecfg,
+  $pxe_menu       = $quartermaster::params::pxe_menu,
+  $tftpboot       = $quartermaster::params::tftpboot,
+  $tftp_username  = $quartermaster::params::tftp_username,
+  $tftp_group     = $quartermaster::params::tftp_group,
+  $tftp_filemode  = $quartermaster::params::tftp_filemode,
+  $wwwroot        = $quartermaster::params::wwwroot,
+  $www_username   = $quartermaster::params::www_username,
+  $www_group      = $quartermaster::params::www_group,
+  $file_mode      = $quartermaster::params::file_mode,
+
 ) inherits quartermaster::params {
 
+  tftp::file{'hp_spp':
+    ensure => directory,
+  } ->
+
+  file{"${wwwroot}/HP_SPP":
+    ensure => directory,
+  } ->
+  file{"${wwwroot}/HP_SPP/iso":
+    ensure => directory,
+  } ->
+  staging::file{'HP_SPP_ISO':
+    source => $hp_spp_iso_complete_url_location,
+    target => "${wwwroot}/HP_SPP/iso/${hp_spp_iso_name}",
+    require =>  File["${wwwroot}/HP_SPP/iso"],
+  }
 
 }
