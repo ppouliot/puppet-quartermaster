db1578d6c5b9aeeeb1a9ab329f1ebaa520f43a39
primeministerp <primeministerpete@gmail.com>
2015-09-08 10:58:04 -0400

    adding url

--- manifests/hp_spp.pp
+++ manifests/hp_spp.pp
@@ -1,4 +1,9 @@
 # == Class: quartermaster::hp_spp
 #
-class quartermaster::hp_spp {
+class quartermaster::hp_spp (
+  $hp_spp_iso_complete_url_location = 'ftp://ftp.hp.com/pub/softlib2/software1/cd-generic/p67859018/v108240/SPP2015040.2015_0407.5.iso',
+  $hp_spp_iso_name = 'SPP2015040.2015_0407.5.iso',
+) inherits quartermaster::params {
+
+
 }
