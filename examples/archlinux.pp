18b701460fa601af43285be74c999b94fdb71025
Peter J. Pouliot <peter@pouliot.net>
2017-03-28 12:05:51 -0400

    adding example for testing archlinux

---
+++ examples/archlinux.pp
@@ -0,0 +1,9 @@
+# archlinux
+quartermaster::pxelinux{ 'archlinux-2016.12.01-i686' }
+quartermaster::pxelinux{ 'archlinux-2016.12.01-x86_64' }
+quartermaster::pxelinux{ 'archlinux-2017.01.01-i686' }
+quartermaster::pxelinux{ 'archlinux-2017.01.01-x86_64' }
+quartermaster::pxelinux{ 'archlinux-2017.02.01-i686' }
+quartermaster::pxelinux{ 'archlinux-2017.02.01-x86_64' }
+quartermaster::pxelinux{ 'archlinux-latest-i686' }
+quartermaster::pxelinux{ 'archlinux-latest-x86_64' }
