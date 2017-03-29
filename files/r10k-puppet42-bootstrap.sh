4c9519b3e4cf449659adc2214aab89a9986769ff
Peter J. Pouliot <peter@pouliot.net>
2017-03-29 18:10:56 +0000

    adding bits

--- files/r10k-puppet42-bootstrap.sh
+++ files/r10k-puppet42-bootstrap.sh
@@ -4,10 +4,10 @@ echo -n "creating framework to support automated puppet/r10k workflows"
 echo -n "- installing git"
 apt-get update -y && apt-get install -y git
 echo -n "- changing to /etc"
-cd /etc/
-rm -rf /etc/puppet
-echo -n "- Retrieving Base /etc/puppet"
-git clone https://github.com/ppouliot/puppet-etc_puppet puppet
+#cd /etc/
+#rm -rf /etc/puppet
+#echo -n "- Retrieving Base /etc/puppet"
+#git clone https://github.com/ppouliot/puppet-etc_puppet puppet
 
 echo -n "- Installing PuppetLabs Repositories"
 cd /tmp
@@ -16,14 +16,14 @@ cd /tmp
 release=`lsb_release -c | awk '{print $2}'`
 
 echo '### INSTALLING PUPPETLABS APT REPO ###'
-wget http://apt.puppetlabs.com/puppetlabs-release-PC1-$release.deb; dpkg -i puppetlabs-release-PC1-$release.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-pc1-$release.deb; dpkg -i puppetlabs-release-pc1-$release.deb
 if [ $? $test -eq 1 ]; then
    echo "Could not find puppetlabs release for $release.  Trying alternative."
-   wget http://apt.puppetlabs.com/puppetlabs-release-PC1-precise.deb; dpkg -i puppetlabs-release-PC1-precise.deb
+   wget http://apt.puppetlabs.com/puppetlabs-release-pc1-precise.deb; dpkg -i puppetlabs-release-pc1-precise.deb
 fi
 
 echo '### UPDATING AND INSTALLING NECESSARY PACKAGES ###'
-apt-get update -y && apt-get install -y openssh-server puppet_agent ruby ruby-dev python-software-properties
+apt-get update -y && apt-get install -y openssh-server puppet-agent ruby ruby-dev python-software-properties
 
 
 echo "### INSTALLING  R10K ###"
