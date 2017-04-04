6e0c6300cdcea0cadbb9662a6073c3599b4729eb
Peter J. Pouliot <peter@pouliot.net>
2017-04-04 22:54:24 +0000

    changing script order

---
+++ examples/windows.pp
@@ -0,0 +1,74 @@
+# Windows
+# 
+#quartermaster::windowsmedia{
+#  ""en_windows_8_"enterprise_x86_dvd_917587.iso":
+#  activationkey => "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"}
+# }
+
+# windows
+quartermaster::windowsmedia{
+  "en_windows_server_2012_x64_dvd_915478.iso:"
+    activationkey: >
+      ENC[PKCS7,MIIBiQYJKoZIhvcNAQcDoIIBejCCAXYCAQAxggEhMIIBHQIBADAFMAACAQEw
+      DQYJKoZIhvcNAQEBBQAEggEAw3/PTe+9GUsT+Gw82WXXBn6YNFv04o2Pi5kb
+      VMQx4MfBwCT8dNj1mVrBI4ciWiZK2zFBteuapA8rN1aP8iMZ17DQ//qvpCJo
+      lkh23Opckl/mC36GoN3IEgFMJR01GZZaa3HOsPIOu/hSw0HRpVm+Kh10ko59
+      xi9x3oxGyM31BtJaZrlP+x/9rI++v4b3PObh61zIfBwVcAyt6t9KyVNzQWYc
+      EProabsLSbKLasR9CWRHcFF2R9E81ZIPSn8IwZg8P2FwIy5jxAA9QTnA/D6/
+      uIcUrPN5gVOnVdrT4+wIsmQvTCGcOymkBzytU2f6Dwbd/lWzUQW8vU7sIdJZ
+      7jnTZzBMBgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBBJbwdNH/1OTsRihsz2
+     KmLNgCD8fm5BqxIc4vfKh7KGMeJ2sa0a4ft5tfR7C+BHRPDhEw==]
+quartermaster::windowsmedia{
+  "en_microsoft_hyper-v_server_2012_x64_dvd_915600.iso:"
+    activationkey: undef
+quartermaster::windowsmedia{
+  "en_windows_8_enterprise_x64_dvd_917522.iso:"
+    activationkey: >
+      ENC[PKCS7,MIIBiQYJKoZIhvcNAQcDoIIBejCCAXYCAQAxggEhMIIBHQIBADAFMAACAQEw
+      DQYJKoZIhvcNAQEBBQAEggEATnUsAfhdhTEzeA1Fn9GSaRvTpt1+5BN6UGkw
+      GKGqRVKbdEiQNRr1HLE33XzjjXiTw9pC1Jiq1KTktmpgvofqCxne5LqUWu2T
+      Yt7O8mfYlpZGOzNzQOIWBB0iLCgTiOTe11t9G2ovhIwsuUTMgCI5HTmbyZgv
+      3I0HtWxNK/f5T3FQl3cFkkttgxnY9uf8tT07wIMa/satE01hP1Z38b/wZHUm
+      b/hJU9RIQQFCRxOvyHKTfuhOC9sfMfYE55Hqu7j1vIn++pqG0tM+CCZ2FV/7
+      XNkLJ3BEO7UTAq86/0UxHFUWS6kY5hIq+tnnSstGJb4YGzw6z2SBxE3Kzsbe
+      nHShIzBMBgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBDDxA2nQEu1LYgl5Pt5
+      0wlIgCDDQJMMVEfwACLsAPIBMTiW5hMS/pLmu+3nPiWuCSbqRA==]
+quartermaster::windowsmedia{
+  "en_windows_8_enterprise_x86_dvd_917587.iso:"
+    activationkey: >
+      ENC[PKCS7,MIIBiQYJKoZIhvcNAQcDoIIBejCCAXYCAQAxggEhMIIBHQIBADAFMAACAQEw
+      DQYJKoZIhvcNAQEBBQAEggEAdNxlQn2fimUxWNqBpmcsHzrW4LvMsb4xN5PH
+      7za2AGYgRZx8YF18S4Xg0wqO9TJ1akIZWKEhvK9GyfMB1Mk4XxmzIwYQqBv6
+      KOtbsqwkt8rnzaicc8+PJSRVpbqewrcbb3n7i3PU3Vj9S5W42B5B2qJy/hpr
+      NqYsFMAqlNtGagMqoYL8h0V9f9BpnQE0IHjntWPm1+aD9basahuKTXRH0SYp
+      HD8H/t9UcYOtX4CqV1Owi4TmFha7w24ex0jNjxatP9UYU5dVmqIshr5GFHP9
+      DpI703N4tQF8HY8RN+ok1bCw8zjC+OiQSit6bh1+TE3BljNx7kVoyNXi9F7E
+      B8dRJjBMBgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBDJe9s9Z6Z7DxhwLt+h
+      CKkJgCDYacATaFjxl5lQQmlJhACEJtNxpdb/B4NojhZbJpN96g==]
+quartermaster::windowsmedia{
+  "en_microsoft_hyper-v_server_2012_r2_x64_dvd_2708236.iso:"
+    activationkey: undef
+quartermaster::windowsmedia{
+  "en_windows_server_2012_r2_x64_dvd_2707946.iso:"
+    activationkey: >
+      ENC[PKCS7,MIIBiQYJKoZIhvcNAQcDoIIBejCCAXYCAQAxggEhMIIBHQIBADAFMAACAQEw
+      DQYJKoZIhvcNAQEBBQAEggEAnDJsKUg1kfAcBjNpNAwuIy7Lzprx32zhXpK3
+      7WQBJnJqQ0h7QgUAvy9vjE0KS02I7a7m5qIUsLbAOQd8Kl+c8417UzvogqBB
+      NR+r6uubzm+TB0pcK+Lvw9ZeIt21zbEe8O2ZAhqAgVIhBQZTu5vuyH361mzv
+      bcXSbfCMLb38exDDNUjptB7Q3qh5mhjYVWfwypvNxEJFV9a1kpLVSH95q6Mx
+      JYQMlFDeFgdmWfiacnD3bQPva0UAZdyYx35xz5lA6ODylkGTmVdIj58BsaRu
+      DmwXhwqfQ+3930qhyJcRT4WtygulAM1LoqW+d02ZoG8HMqsqLapic342DrNT
+      rzqs1TBMBgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBDtF/oq6ZUtQa2r/4C3
+      QX82gCBX4pENNByOlb3IjbY74H2nJkhs3KDbcuZUCFe4VlUyXw==]
+quartermaster::windowsmedia{
+  "en_windows_8_1_enterprise_x64_dvd_2971902.iso:"
+    activationkey: >
+      ENC[PKCS7,MIIBiQYJKoZIhvcNAQcDoIIBejCCAXYCAQAxggEhMIIBHQIBADAFMAACAQEw
+      DQYJKoZIhvcNAQEBBQAEggEAAjyCk9LoFkft8k4ZG+b95oAFi0LpshLO3G8p
+      Y1654p8RtDh9g+/cSYbTQt15QK4Igfc6VXiJ+z39UCSLL1EhSy6duTbYV3rm
+      rfkuMY1CfqYK+ufglg64nP4AFQtrz0LeTy0yeVXzSU1hT1B7sOxDykKhVTlY
+      mrU3/WPzwwISKbd99e9K6oSLDGhKMf9ZGPViDDZeUgLYwhK5BHyCc0bnRUxp
+      u+SP3PsAzcw2TBUXvcuY1FYgejhc+Y2QK2g0sRYs7IgkOaPkkKmynHmW5Ct2
+      5+ei7nf0crO0B/Fn3uMEAlrMoTQIYUM18QMeQDNf20FVhVkx89P6V5tPZ8Qd
+      MaLuhDBMBgkqhkiG9w0BBwEwHQYJYIZIAWUDBAEqBBByBnMgXD8v5dxkh0c/
+      NwJAgCAclAeBo8jnOT4kwZKm6rqJI7fFrXtk5fTTjFvuI6/sWQ==]
