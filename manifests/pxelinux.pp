792a04a8a59e3b399a099153041170ca1e6ae2a9
Peter J. Pouliot <peter@pouliot.net>
2017-11-30 10:16:26 -0500

    updating matchbox

--- manifests/pxelinux.pp
+++ manifests/pxelinux.pp
@@ -720,230 +720,260 @@ define quartermaster::pxelinux (
       }
       # Begin Examples
       file{[
-        "/var/lib/matchbox/examples/${release}",
-        "/var/lib/matchbox/examples/${release}/groups",
-        "/var/lib/matchbox/examples/${release}/groups/simple",
-        "/var/lib/matchbox/examples/${release}/groups/simple-install",
-        "/var/lib/matchbox/examples/${release}/groups/etcd3",
-        "/var/lib/matchbox/examples/${release}/groups/etcd3-install",
-        "/var/lib/matchbox/examples/${release}/groups/grub",
-        "/var/lib/matchbox/examples/${release}/groups/bootkube",
-        "/var/lib/matchbox/examples/${release}/groups/bootkube-install",
-        "/var/lib/matchbox/examples/${release}/profiles",
-#        "/var/lib/matchbox/examples/${release}/ignition",
+        "/var/lib/matchbox/examples/${coreos_version}",
+        "/var/lib/matchbox/examples/${coreos_version}/groups",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/simple",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/simple-install",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/grub",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube",
+        "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube-install",
+        "/var/lib/matchbox/examples/${coreos_version}/profiles",
+#        "/var/lib/matchbox/examples/${coreos_version}/ignition",
       ]:
         ensure  => directory,
         owner   => 'matchbox',
         group   => 'matchbox',
       }
-      file{ "/var/lib/matchbox/examples/${release}/ignition":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/ignition":
         ensure  => directory,
         owner   => 'matchbox',
         group   => 'matchbox',
         recurse => true,
         source  => 'puppet:///modules/quartermaster/coreos/matchbox/ignition',
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/grub/default.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/grub/default.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/grub/default.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/simple/default.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/simple/default.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/simple/default.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/simple-install/simple.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/simple-install/simple.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/simple-install/simple.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/simple-install/install.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/simple-install/install.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/simple-install/install.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3/gateway.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3/gateway.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3/gateway.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3/node1.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3/node1.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3/node1.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3/node2.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3/node2.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3/node2.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3/node3.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3/node3.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3/node3.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3-install/gateway.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install/gateway.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3-install/gateway.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3-install/node1.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install/node1.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3-install/node1.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3-install/node2.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install/node2.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3-install/node2.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3-install/node3.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install/node3.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3-install/node3.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/etcd3-install/install.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/etcd3-install/install.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/etcd3-install/install.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube/node1.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube/node1.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube/node1.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube/node2.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube/node2.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube/node2.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube/node3.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube/node3.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube/node3.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube-install/node1.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube-install/node1.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube-install/node1.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube-install/node2.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube-install/node2.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube-install/node2.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube-install/node3.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube-install/node3.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube-install/node3.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/groups/bootkube-install/install.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/groups/bootkube-install/install.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/groups/bootkube-install/install.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/simple.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/simple.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/simple.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/simple-install.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/simple-install.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/simple-install.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/grub.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/grub.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/grub.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/etcd3.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/etcd3.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/etcd3.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/etcd3-gateway.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/etcd3-gateway.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/etcd3-gateway.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/bootkube-worker.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/bootkube-worker.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/bootkube-worker.json.erb'),
       }
-      file{ "/var/lib/matchbox/examples/${release}/profiles/bootkube-controller.json":
+      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/bootkube-controller.json":
         ensure  => file,
         owner   => 'matchbox',
         group   => 'matchbox',
         content => template('quartermaster/matchbox/profiles/bootkube-controller.json.erb'),
       }
+# Below is commented out until bits are properly cleaned up.
       # matchbox profiles grub.json
-      notice("matchbox/profiles/${release}.json")
-      file{ "/var/lib/matchbox/profiles/${release}.json":
-        ensure  => file,
-        owner   => 'matchbox',
-        group   => 'matchbox',
-        content => template('quartermaster/matchbox/profiles.channel.json.erb'),
-      }
+#
+#      file{ "/var/lib/matchbox/groups/${release}-install.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/groups.channel-install.json.erb'),
+#      }
+#      notice("matchbox/profiles/${release}-install.json")
+#      file{ "/var/lib/matchbox/profiles/${release}-install.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.channel-install.json.erb'),
+#      }
+#      notice("matchbox/groups/${release}.json")
+#      file{ "/var/lib/matchbox/groups/${release}.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/groups.channel.json.erb'),
+#      }
+      # matchbox groups etcd3-install.json
+#      file{ "/var/lib/matchbox/groups/etcd3-${release}-install.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/groups.etcd3-install.json.erb'),
+#      }
+      # Begin Examples
+#      notice("matchbox/profiles/${release}.json")
+#      file{ "/var/lib/matchbox/profiles/${release}.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.channel.json.erb'),
+#      }
       # matchbox profiles grub.json
-      file{ "/var/lib/matchbox/examples/${release}/profiles/grub-${release}.json":
-        ensure  => file,
-        owner   => 'matchbox',
-        group   => 'matchbox',
-        content => template('quartermaster/matchbox/profiles.grub.json.erb'),
-      }
+#      file{ "/var/lib/matchbox/examples/${coreos_version}/profiles/grub-${release}.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.grub.json.erb'),
+#      }
 
       # matchbox profiles etcd3.json
-      file{ "/var/lib/matchbox/profiles/etcd3-${release}.json":
-        ensure  => file,
-        owner   => 'matchbox',
-        group   => 'matchbox',
-        content => template('quartermaster/matchbox/profiles.etcd3.json.erb'),
-      }
+#      file{ "/var/lib/matchbox/profiles/etcd3-${release}.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.etcd3.json.erb'),
+#      }
 
       # matchbox profiles etcd3-gateway.json
-      file{ "/var/lib/matchbox/profiles/etcd3-gateway-${release}.json":
-        ensure  => file,
-        owner   => 'matchbox',
-        group   => 'matchbox',
-        content => template('quartermaster/matchbox/profiles.etcd3-gateway.json.erb'),
-      }
+#      file{ "/var/lib/matchbox/profiles/etcd3-gateway-${release}.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.etcd3-gateway.json.erb'),
+#      }
 
      # profiles install-channel-reboot.json
-      file{ "/var/lib/matchbox/profiles/install-${release}-reboot.json":
-        ensure  => file,
-        owner   => 'matchbox',
-        group   => 'matchbox',
-        content => template('quartermaster/matchbox/profiles.install-channel-reboot.json.erb'),
-      }
+#      file{ "/var/lib/matchbox/profiles/install-${release}-reboot.json":
+#        ensure  => file,
+#        owner   => 'matchbox',
+#        group   => 'matchbox',
+#        content => template('quartermaster/matchbox/profiles.install-channel-reboot.json.erb'),
+#      }
 
     }
   }
