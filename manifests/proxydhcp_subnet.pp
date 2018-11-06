1fade44517be76a0eaea8e28d57ad520f0fc6df9
Peter J. Pouliot <peter@pouliot.net>
2018-11-06 15:54:08 +0000

    updating to use new dnsmasq module

--- manifests/proxydhcp_subnet.pp
+++ manifests/proxydhcp_subnet.pp
@@ -3,14 +3,21 @@ define quartermaster::proxydhcp_subnet(
   $network_ipaddr,
   $network_subnet_mask,
 ) {
-  
-  dnsmasq::dhcp{$name:
-    dhcp_start => "${network_ipaddr},proxy",
-    dhcp_end   => $network_subnet_mask,
-    lease_time => '',
-    netmask    => '',
-    notify     => Service['dnsmasq'],
+
+# new format using saz/dnsmasq
+# #dhcp-range=172.20.230.92,proxy,255.255.255.192,,
+  dnsmasq::conf{"$name":
+    ensure => present,
+    content => "dhcp-range=${network_ipaddr},proxy,${network_subnet_mask},",
   }
+# Old Format using lex/dnsmasq  
+#  dnsmasq::dhcp{$name:
+#    dhcp_start => "${network_ipaddr},proxy",
+#    dhcp_end   => $network_subnet_mask,
+#    lease_time => '',
+#    netmask    => '',
+#    notify     => Service['dnsmasq'],
+#  }
 
 
 }
