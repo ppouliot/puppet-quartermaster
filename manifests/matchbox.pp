311848538972bc947b6ff7e303329c0660eaf1fa
primeministerp <primeministerpete@gmail.com>
2018-12-06 19:24:45 +0000

    updating

--- manifests/matchbox.pp
+++ manifests/matchbox.pp
@@ -9045,51 +9045,51 @@ providers {
       environment => [
         "SAN=DNS.1:${::fqdn},IP.1:${::ipaddress}",
       ],
-      command     => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/cert-gen',
-      cwd         => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/',
+      command     => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/cert-gen",
+      cwd         => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/",
       logoutput   => true,
       timeout     => '0',
       user        => 'root',
       creates     => [
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/certs',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/crl',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/newcerts',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/serial',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/index.txt',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/index.txt.attr',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/ca.crt',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/ca.key',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/server.crt',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/server.key',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/client.crt',
-      '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/client.key',
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/certs",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/crl",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/newcerts",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/serial",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/index.txt",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/index.txt.attr",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/ca.crt",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/ca.key",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/server.crt",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/server.key",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/client.crt",
+      "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/client.key",
       ],
     } ->
     # Generated Certs to /etc ca.crt server.crt server.key
     file{'/etc/matchbox/ca.crt':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/ca.crt',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/ca.crt",
     } ->
     file{'/etc/matchbox/server.crt':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/server.crt',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/server.crt",
     } ->
     file{'/etc/matchbox/server.key':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/server.key',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/server.key",
     } ->
     # Add client Certs to /root/.matchbox
     file{'/root/.matchbox/client.key':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/client.key',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/client.key",
     } ->
     file{'/root/.matchbox/client.crt':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/client.crt',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/client.crt",
     } ->
     file{'/root/.matchbox/ca.crt':
       ensure => file,
-      source => '/home/matchbox/matchbox-v0.6.1-linux-amd64/scripts/tls/ca.crt',
+      source => "/home/matchbox/matchbox-v${quartermaster::matchbox_version}-linux-amd64/scripts/tls/ca.crt",
     }
     if ( $facts[os][distro][id] == 'Ubuntu' ) and ( $facts[os][distro][release][major] == '14.04'){
       package{'systemd':
