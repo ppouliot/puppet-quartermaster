d1e3446c24897e9511920e4265b35d2f367ad0bd
primeministerp <primeministerpete@gmail.com>
2018-12-12 17:17:12 +0000

    fixing typo

--- manifests/pxelinux.pp
+++ manifests/pxelinux.pp
@@ -1272,7 +1272,7 @@ define quartermaster::pxelinux (
     'ISO Required instead of URL':{
       case $unzip_iso {
         'true':{
-          if ! defined (Staging::File["${name}-boot.iso"]){
+          if ! defined (Staging::deploy["${name}-boot.iso"]){
             staging::deploy{"${name}-boot.iso":
               source  => $boot_iso_url,
               target  => "/srv/quartermaster/${distro}/ISO",
@@ -1338,8 +1338,8 @@ define quartermaster::pxelinux (
         }
       }
     }
-   # End New Embedding for unzip_iso
     }
+    # End New Embedding for unzip_iso
     'No URL Specified':{
       warning("No URL is specified for ${name}")
     }
